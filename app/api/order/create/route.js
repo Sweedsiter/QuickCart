99da4cfff8c5ca0afe08d8229052012d41c8ede1
Sweedsier <d27saitunlu@gmail.com>
2025-04-27 01:45:33 +0700

    3

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,7 +1,6 @@
 import { inngest } from "@/config/inngest";
 import Product from "@/models/Product";
 import User from "@/models/User";
-
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -24,8 +23,6 @@ export async function POST(request) {
 
             return await acc + product.offerPrice * item.quantity;
         }, 0)
-
-    
 
         await inngest.send({
             name: 'order/created',
--- config/inngest.js
+++ config/inngest.js
@@ -63,7 +63,7 @@ export const syncUserDeletion = inngest.createFunction(
 // Inngest Function to create user's order database
 
 export const createUserOrder = inngest.createFunction({
-    id: 'create-user-order',
+    id: 'create-user-orders',
     batchEvents: {
         maxSize: 5,
         timeout: '5s'
