3c0f4082bcadab155764c7a77c6fd84f52bc7105
Sweedsier <d27saitunlu@gmail.com>
2025-04-27 10:44:14 +0700

    add img

--- models/Order.js
+++ models/Order.js
@@ -5,9 +5,9 @@ const orderSchema = new mongoose.Schema({
     email :{ type: String, required: true},
     items: [{
         product: {type: String, required: true, ref: 'product'},
-        quantity: {type: Number, required: true},   
-        itemsWithImages: { type: String, required: true } // Add image field for the product
+        quantity: {type: Number, required: true},  
     }],
+    itemsWithImages: { type: String, required: true }, // Add image field for the product
     amount: { type: Number, required: true},
     address: {type: String, ref: 'address' ,required: true},
     status: { type: String, required: true, default: 'Order Placed' },
