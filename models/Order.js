338db8fde75262a9f082ed30efc069503bf87924
Sweedsier <d27saitunlu@gmail.com>
2025-04-27 10:54:28 +0700

    add imge

--- models/Order.js
+++ models/Order.js
@@ -1,20 +1,28 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-    userId: { type: String, required: true, ref: 'user'},
-    email :{ type: String, required: true},
+    userId: { type: String, required: true, ref: 'user' },
+    email: { type: String, required: true },
     items: [{
-        product: {type: String, required: true, ref: 'product'},
-        quantity: {type: Number, required: true},  
+        product: { type: String, required: true, ref: 'product' },
+        quantity: { type: Number, required: true },
     }],
-    itemsWithImages: { type:  Array, required: true }, // Add image field for the product
-    amount: { type: Number, required: true},
-    address: {type: String, ref: 'address' ,required: true},
+    itemsWithImages: [
+        {
+            image: { type: Array, required: true },
+            product: { type: String, required: true, ref: 'product' },
+            quantity: { type: Number, required: true },
+        }
+    ], // Add image field for the product
+    amount: { type: Number, required: true },
+    address: { type: String, ref: 'address', required: true },
     status: { type: String, required: true, default: 'Order Placed' },
-    date : { type: Number, required: true}
+    date: { type: Number, required: true }
 })
 
-const Order = mongoose.models.order || mongoose.model('order',orderSchema)
+const Order = mongoose.models.order || mongoose.model('order', orderSchema)
 
 
 export default Order
+
+
