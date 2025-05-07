98504b84436fbf9f0cf7d5bf655e31f23b6bc5fe
Sweedsier <d27saitunlu@gmail.com>
2025-05-07 22:14:00 +0700

    HomeProduct Waithing edit

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 import { useAppContext } from "@/context/AppContext";
+import Loading from "@/components/Loading";
 
 const HomeProducts = () => {
 
   const { products, router } = useAppContext()
-  
+
 
   return (
     <div className="flex flex-col items-center pt-14">
@@ -13,9 +14,12 @@ const HomeProducts = () => {
         <p className="text-2xl font-medium text-left w-full">Popular products</p>
         <div className="w-24 h-0.5 bg-orange-600 rounded-full"></div>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-6 pb-14 w-full h-[600px] overflow-x-hidden overflow-y-auto">
-        {products.map((product, index) => <ProductCard key={index} product={product} />)}
-      </div>
+      {
+        products.length === 0 ? <div className="h-[400px]"><Loading /></div> :
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-6 pb-14 w-full h-[600px] overflow-x-hidden overflow-y-auto">
+            {products.map((product, index) => <ProductCard key={index} product={product} />)}
+          </div>
+      }
       <button onClick={() => { router.push('/all-products') }} className="px-12 py-2.5 border rounded text-gray-500/70 hover:bg-slate-50/90 hover:text-black transition mt-16">
         กดดูทั้งหมด....
       </button>
