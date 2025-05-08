e65666656199aed65e0df281822114d0a8ee87e3
Sweedsier <d27saitunlu@gmail.com>
2025-05-08 12:02:17 +0700

    add input seller product list

--- app/seller/product-list/page.jsx
+++ app/seller/product-list/page.jsx
@@ -14,6 +14,10 @@ const ProductList = () => {
 
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(true)
+  const [searchTerm, setSearchTerm] = useState(""); // State for search input
+
+ 
+  console.log(searchTerm)
 
 
   // Delete product function
@@ -75,15 +79,19 @@ const ProductList = () => {
     <div className="flex-1 min-h-screen flex flex-col justify-between">
       {loading ? <Loading /> : <div className="w-full md:p-10 p-4">
         <h2 className="pb-4 text-lg font-medium">All Product</h2>
+        <input
+          type="text"
+          placeholder="ค้นหาตามชื่อ..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="border border-gray-300 rounded px-4 py-2 w-fit mb-4"
+        />
         <div className="flex flex-col items-center w-full overflow-hidden rounded-md bg-white border border-gray-500/20">
           <table className=" table-fixed w-full overflow-hidden">
             <thead className="text-gray-900 text-sm text-left">
               <tr>
                 <th className="w-2/3 md:w-2/5 px-4 py-3 font-medium truncate">Product</th>
                 <th className="px-4 py-3 font-medium truncate max-sm:hidden">Category</th>
-                <th className="px-4 py-3 font-medium truncate">
-                  Price
-                </th>
+                <th className="px-4 py-3 font-medium truncate">Price</th>
                 <th className="px-4 py-3 font-medium truncate max-sm:hidden">Action</th>
                 <th className="px-4 py-3 font-medium truncate max-sm:hidden">Delete</th>
                 <th className="px-4 py-3 font-medium truncate max-sm:hidden">Update</th>
@@ -119,10 +127,10 @@ const ProductList = () => {
                       />
                     </button>
                   </td>
-                  <td className="px-4 py-3" > 
-                     <button onClick={oneDelete(product._id)} className="flex items-center gap-1 px-1.5 md:px-3.5 py-2 bg-red-600 text-white rounded-md">
-                     <span className="hidden md:block">Delete</span>
-                  </button></td>
+                  <td className="px-4 py-3" >
+                    <button onClick={oneDelete(product._id)} className="flex items-center gap-1 px-1.5 md:px-3.5 py-2 bg-red-600 text-white rounded-md">
+                      <span className="hidden md:block">Delete</span>
+                    </button></td>
                   <td className="px-4 py-3" >
                     <button onClick={handleUpdate(product._id)} className="flex items-center gap-1 px-1.5 md:px-3.5 py-2 bg-green-600 text-white rounded-md">
                       <span className="hidden md:block">Update</span>
