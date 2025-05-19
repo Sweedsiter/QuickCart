2bad75357ab5e502abfdc1721cf8316d720c16a5
Sweedsier <d27saitunlu@gmail.com>
2025-05-19 11:42:56 +0700

    add user

---
+++ app/seller/all-user/page.jsx
@@ -0,0 +1,15 @@
+'use client';
+import Footer from "@/components/seller/Footer";
+
+
+
+
+export default function All_user(){
+    return (
+        <div className="flex-1 h-screen overflow-scroll flex flex-col justify-between text-sm">       
+            
+
+            <Footer />
+        </div>
+    );
+}
