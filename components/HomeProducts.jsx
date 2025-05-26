17b04fa8f40e53d7ca3bf56439b2f9a1f5267aea
Sweedsier <d27saitunlu@gmail.com>
2025-05-26 12:02:41 +0700

    Load edit

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ProductCard from "./ProductCard";
 import { useAppContext } from "@/context/AppContext";
 import Loading from "@/components/Loading";
@@ -16,33 +16,30 @@ const HomeProducts = () => {
 
   return (
     <>
-    {      isLoading ? (
-     <div className="md:h-[400px] h-[300px]"><Loading /></div>
-    ) : (
-      <div className="flex flex-col items-center pt-14">
-      <div className="flex flex-col w-full mb-8 text-3xl ">
-        <p className="text-2xl font-medium text-left w-full">Popular products</p>
-    
-        <div className="w-24 h-0.5 bg-orange-600 rounded-full"></div>
-      </div>
-      {/* <p onClick={() => { router.push('/all-products') }} className="underline text-blue-600 hover:text-lg cursor-pointer">กดดูทั้งหมด....</p> */}
-      {
-        products.length === 0 ? <div className="md:h-[400px] h-[300px]"><Loading /></div> :
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 flex-col items-center gap-6 mt-6 pb-14 w-full h-[340px]  md:h-[500px] overflow-x-hidden overflow-y-auto">
-            {products.map((product, index) => <ProductCard key={index} product={product} />)}
+      {isLoading ? (
+        <div className="md:h-[400px] h-[300px]"><Loading /></div>
+      ) : (
+        <div className="flex flex-col items-center pt-14">
+          <div className="flex flex-col w-full mb-8 text-3xl ">
+            <p className="text-2xl font-medium text-left w-full">Popular products</p>
+
+            <div className="w-24 h-0.5 bg-orange-600 rounded-full"></div>
           </div>
-      }      
+          {/* <p onClick={() => { router.push('/all-products') }} className="underline text-blue-600 hover:text-lg cursor-pointer">กดดูทั้งหมด....</p> */}
+          {
+            products.length === 0 ? <div className="md:h-[400px] h-[300px]"><Loading /></div> :
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 flex-col items-center gap-6 mt-6 pb-14 w-full h-[340px]  md:h-[500px] overflow-x-hidden overflow-y-auto">
+                {products.map((product, index) => <ProductCard key={index} product={product} />)}
+              </div>
+          }
           <button
-        onClick={handleClick}
-        disabled={isLoading}
-        className={`px-12 py-2.5 border rounded transition mt-6
-          ${isLoading ? 'bg-gray-100 text-gray-400 cursor-not-allowed' : 'text-gray-500/70 hover:bg-slate-50/90 hover:text-black'}
-        `}
-      >
-        {isLoading ? 'กำลังโหลด...' : 'กดดูทั้งหมด....'}
-      </button>
-    </div>
-    )}
+            onClick={handleClick}
+            className="px-12 py-2.5 border rounded transition mt-6 text-gray-500/70 hover:bg-slate-50/90 hover:text-black"
+          >
+            กดดูทั้งหมด....
+          </button>
+        </div>
+      )}
     </>
   );
 };
