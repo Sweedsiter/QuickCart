23c17208dff899d2b3d5ae957ba5a1ca0a8b3623
Sweedsier <d27saitunlu@gmail.com>
2025-06-08 19:42:11 +0700

    q

--- app/all-products/page.jsx
+++ app/all-products/page.jsx
@@ -62,7 +62,7 @@ const AllProducts = () => {
             className="border border-gray-300 rounded-md p-2 md:ml-4 w-full md:w-fit"
           />
           <span className="md:ml-4 block w-full my-2">ทั้งหมด : {filteredProducts.length} ลาย</span>
-          <span>รูปไม่แสดง กรุณาให้กดที่รูป</span>
+          <span>รูปไม่แสดงกรุณาให้กดที่รูป</span>
         </div>
 
         {/* Product Grid */}
